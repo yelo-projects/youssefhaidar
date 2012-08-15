b34f21a9307535a5596f2907219bd61798522f44
Xananax <xananax@yelostudio.com>
2012-08-15 13:23:51 +0300

    updated js build script

--- site/javascript/build.sh
+++ site/javascript/build.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 
-./minify.sh jquery.easing.js main.js
+./minify.sh jquery.easing.js jquery.tinysort.min.js jquery.mousewheel.js jquery.scrollTo-1.4.2-min.js jquery.localscroll-1.2.7-min.js jquery.jscrollpane.min.js main.js 
